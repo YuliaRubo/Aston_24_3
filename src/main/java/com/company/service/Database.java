4b42c7459bba441e773a593330fe68e35b54ade8
Ulya Ivancova <ulya-rubo@mail.ru>
2023-03-28 18:52:15 +0200

    nothing

--- src/main/java/com/company/service/Database.java
+++ src/main/java/com/company/service/Database.java
@@ -1,56 +1,63 @@
-package com.company.service;
-
-
-import java.sql.*;
-
-public  class Database extends ConfigDb {
-
-    private  static Connection connection;
-    private static Statement statement;
-
-    public static void getDbConnection(){
-       try {
-           String connectionString = "jdbc:mysql//" + dbHost + ":"
-                   + dbPort + "/" + dbName;
-           try {
-               Class.forName("com.mysql.jdbc.Driver");
-           } catch (ClassNotFoundException e) {
-               e.printStackTrace();
-           }
-           try {
-               connection = DriverManager.getConnection(connectionString, dbUser, dbPass);
-           } catch (SQLException e) {
-               e.printStackTrace();
-           }
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
-
-    }
-    public static void disconnect() {
-        try {
-            statement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        try {
-            connection.close();
-            System.out.println("Disconnected");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public  static ResultSet getResultSet() throws SQLException {
-        return statement.getResultSet();
-    }
-
-    public Statement getStatement() {
-        return statement;
-    }
-
-    public static PreparedStatement getPreparedStatement(String value) throws SQLException {
-        return connection.prepareStatement(value);
-    }
-
-}
+//
+//package com.company.service;
+//
+//
+//import java.sql.*;
+//
+//public  class Database  {
+//
+//    private  static Connection connection;
+//    private static Statement statement;
+//
+//    public static void getDbConnection(){
+//       try {
+//            String dbHost = "Localhost";
+//            String dbPort = "3306";
+//            String dbUser = "root";
+//            String dbPass = "root";
+//            String dbName = "studentoncourse";
+//
+//           String connectionString = "jdbc:mysql//" + dbHost + ":"
+//                   + dbPort + "/" + dbName;
+//           try {
+//               Class.forName("com.mysql.jdbc.Driver");
+//           } catch (ClassNotFoundException e) {
+//               e.printStackTrace();
+//           }
+//           try {
+//               connection = DriverManager.getConnection(connectionString, dbUser, dbPass);
+//           } catch (SQLException e) {
+//               e.printStackTrace();
+//           }
+//       } catch (Exception e) {
+//           e.printStackTrace();
+//       }
+//
+//    }
+//    public static void disconnect() {
+//        try {
+//            statement.close();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            connection.close();
+//            System.out.println("Disconnected");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public  static ResultSet getResultSet() throws SQLException {
+//        return statement.getResultSet();
+//    }
+//
+//    public Statement getStatement() {
+//        return statement;
+//    }
+//
+//    public static PreparedStatement getPreparedStatement(String value) throws SQLException {
+//        return connection.prepareStatement(value);
+//    }
+//
+//}
--- src/main/java/com/company/servlets/HelloServlet.java
+++ src/main/java/com/company/servlets/HelloServlet.java
@@ -1,58 +1,47 @@
 package com.company.servlets;
 
-import com.company.service.Database;
+
 
 import java.io.*;
 import java.sql.*;
+import java.util.Map;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/student")
 public class HelloServlet extends HttpServlet {
-   // private String message;
+    // private String message;
 
-//    public void init() {
+    //    public void init() {
 //        message = "Hello World!";
 //    }
 // Connection connection;
 // PreparedStatement preparedStatement;
 // ResultSet rs;
- String SQL = "SELECT nama, surname FROM STUDENT";
+  //  String SQL = "SELECT nama, surname FROM STUDENT";
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        PrintWriter printWriter=response.getWriter();
+//        response.setContentType("text/html");
+ //       String sp = request.getRequestURI();
+//        //Map<String, Integer>  mapl =
+ //       response.getWriter().write(sp);
+        PrintWriter printWriter = response.getWriter();
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/studentoncourse","root","root");
 
-//            String connectionString = "jdbc:mysql://localhost:3306/studentoncourse";
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-            try {
-               Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentOnCourse", "root", "root");
-               Statement statement = connection.createStatement();
-               ResultSet rs = statement.executeQuery(SQL);
-               while (rs.next()){
-                   printWriter.println(rs.getString("name"));
-                   printWriter.println(rs.getString("surname"));
-               }
-            statement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
+            Statement stmt = con.createStatement();
+            ResultSet rs=stmt.executeQuery("select surname from student");
+            while(rs.next())
+                //System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+                printWriter.write(rs.getInt(rs.getString(3)));
+            con.close();
+        }catch(Exception e)
+        { System.out.println(e);}
     }
 
 
-//        String ps = request.getRequestURI();
-//        response.getWriter().write(ps);
-
-//        PrintWriter out = response.getWriter();
-//        out.println("<html><body>");
-//        out.println("<h1>" + message + "</h1>");
-//        out.println("</body></html>");
-
-
-    public void destroy() {
+        public void destroy () {
+        }
     }
-}
